77c8319878d12c1eeeb5b4d748cbba9fadc75dd3
Kunwar Parth Singh <parthsingh287@gmail.com>
2017-04-18 16:43:59 +0530

    Fully Functional

--- machine.js
+++ machine.js
@@ -1,14 +1,4 @@
-var button = document.getElementsByTagName("input");
-	for(var i=1;i<button.length;i++){
-  		button[i].addEventListener("click", function() {
-    	console.log("Button clicked.");
-    	if(this.value=="="){
-    		document.Calc.Numbers.value=eval(Calc.Numbers.value);
-    		}
-    	else if(this.value=="AC"){
-    		document.Calc.Numbers.value ="";
-    		}
-    	else
-    		document.Calc.Numbers.value += this.value;
-  		});
-  	}
+
+function myfunct(x){
+    document.getElementById("value").value=document.getElementById("value").value+x;
+}
